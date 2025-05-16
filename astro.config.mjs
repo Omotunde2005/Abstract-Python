138212b5577206ee2ff1d68640cf4f5b96113f42
Spinard <edunrilwan@gmail.com>
2025-05-16 13:27:45 +0100

    add: Added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,55 +3,60 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 
+import vercel from '@astrojs/vercel';
+
+
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Abstract-Python',
-			logo: {
-				src: './src/assets/abstract-api-logo.png',
-			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Omotunde2005/abstractpython' }],
-			sidebar: [
-				{
-					label: 'Getting started',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Installation', slug: 'getting_started' },
-						{ label: 'Authentication', slug: 'authentication' },
-						{ label: 'Usage', slug: 'usage'}
-					],
-				},
+  integrations: [
+      starlight({
+          title: 'Abstract-Python',
+          logo: {
+              src: './src/assets/abstract-api-logo.png',
+          },
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Omotunde2005/abstractpython' }],
+          sidebar: [
+              {
+                  label: 'Getting started',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Installation', slug: 'getting_started' },
+                      { label: 'Authentication', slug: 'authentication' },
+                      { label: 'Usage', slug: 'usage'}
+                  ],
+              },
 
-				{
-					label: 'Exchange',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Overview', slug: 'exchange/overview' },
-						{ label: 'Live Exchange', slug: 'exchange/live' },
-						{ label: 'Currency conversion', slug: 'exchange/conversion'},
-						{ label: 'Historical Exchange', slug: 'exchange/historical'},
-					],
-				},
+              {
+                  label: 'Exchange',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Overview', slug: 'exchange/overview' },
+                      { label: 'Live Exchange', slug: 'exchange/live' },
+                      { label: 'Currency conversion', slug: 'exchange/conversion'},
+                      { label: 'Historical Exchange', slug: 'exchange/historical'},
+                  ],
+              },
 
-				{
-					label: 'IP/Geolocation',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Usage', slug: 'ip/ip_api' }
-					],
-				},
+              {
+                  label: 'IP/Geolocation',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Usage', slug: 'ip/ip_api' }
+                  ],
+              },
 
-				{
-					label: 'Value Added Tax (VAT)',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Overview', slug: 'vat/overview' },
-						{ label: 'Validate VAT', slug: 'vat/validate' },
-						{ label: 'Calculate VAT', slug: 'vat/calculate' },
-						{ label: 'VAT categories', slug: 'vat/vat_categories' },
-					],
-				},
-			],
-		}),
+              {
+                  label: 'Value Added Tax (VAT)',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Overview', slug: 'vat/overview' },
+                      { label: 'Validate VAT', slug: 'vat/validate' },
+                      { label: 'Calculate VAT', slug: 'vat/calculate' },
+                      { label: 'VAT categories', slug: 'vat/vat_categories' },
+                  ],
+              },
+          ],
+      }),
 	],
+
+  adapter: vercel(),
 });
